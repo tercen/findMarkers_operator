2b0ab10c2196b3266ff5c92a2f042a64eea33836
Filipe Tavares-Cadete <filipe@cadete.eu>
2020-10-25 16:20:43 +0000

    First draft.

--- main.R
+++ main.R
@@ -1,9 +1,26 @@
 library(tercen)
 library(dplyr)
+library(scran)
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
-  ctx$addNamespace() %>%
+ctx <- tercenCtx()
+
+cluster_to_evaluate <- as.integer(ctx$op.value('cluster_to_evaluate'))
+top_genes <- as.integer(ctx$op.value('top_genes'))
+
+logged_count_matrix <- ctx$as.matrix()
+rownames(logged_count_matrix) <- ctx$rselect()[[1]]
+
+clusters <- ctx$cselect()[[1]]
+
+markers_detected <- findMarkers(logged_count_matrix, clusters)
+
+genes_to_select <- markers_detected[[cluster_to_evaluate]] %>%
+  as_tibble(rownames = "gene_id") %>%
+  dplyr::filter(Top <= top_genes)
+
+heatmap(genes_to_select %>%
+          select(starts_with("log")) %>%
+          as.matrix())
+
+ctx$addNamespace() %>%
   ctx$save()
--- workspace.R
+++ workspace.R
@@ -1,15 +1,32 @@
 library(tercen)
 library(dplyr)
+library(scran)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "f6d7883d26d906b1a8c4a3800d0616d4")
+options("tercen.stepId"     = "851a7d4e-dc7d-4bbb-8d28-6615d23ec1d2")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
-  ctx$addNamespace() %>%
+cluster_to_evaluate <- 5
+top_genes <- 20
+
+ctx <- tercenCtx()
+
+logged_count_matrix <- ctx$as.matrix()
+rownames(logged_count_matrix) <- ctx$rselect()[[1]]
+
+clusters <- ctx$cselect()[[1]]
+
+markers_detected <- findMarkers(logged_count_matrix, clusters)
+
+genes_to_select <- markers_detected[[cluster_to_evaluate]] %>%
+  as_tibble(rownames = "gene_id") %>%
+  dplyr::filter(Top <= top_genes)
+
+heatmap(genes_to_select %>%
+          select(starts_with("log")) %>%
+          as.matrix())
+
+ctx$addNamespace() %>%
   ctx$save()
