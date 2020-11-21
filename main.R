92864bec8f13ffe327e94262b0a2f7691d242350
Filipe Tavares-Cadete <filipe@cadete.eu>
2020-11-21 17:09:00 +0000

    Trying new output format

--- main.R
+++ main.R
@@ -5,25 +5,20 @@ library(scran)
 
 ctx <- tercenCtx()
 
-cluster_to_evaluate <- as.integer(ctx$op.value('cluster_to_evaluate'))
-top_genes <- as.integer(ctx$op.value('top_genes'))
-
 logged_count_matrix <- ctx$as.matrix()
-rownames(logged_count_matrix) <- ctx$rselect()[[1]]
 
 clusters <- ctx$cselect()[[1]]
 
 markers_detected <- findMarkers(logged_count_matrix, clusters)
 
-genes_to_select <- markers_detected[[cluster_to_evaluate]] %>%
-  as_tibble(rownames = "gene_id") %>%
-  dplyr::filter(Top <= top_genes)
+output_frame <- lapply(names(markers_detected),
+                       function(x) markers_detected[[x]] %>%
+                         as_tibble(rownames = ".ri") %>%
+                         mutate(cluster = x,
+                                .ri = as.integer(.ri) - 1) %>%
+                         select(cluster, .ri, FDR)) %>%
+  bind_rows()
 
-output_frame <- genes_to_select %>%
-  pivot_longer(c(starts_with("logFC")),
-               names_to = "comparison",
-               values_to = "logFC") %>%
-  select(-Top, -p.value, -FDR)
-
-ctx$addNamespace() %>%
+ctx$addNamespace(output_frame) %>%
   ctx$save()
+
