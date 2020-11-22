338fb8df1ecd69cbf72111ea5511ee26d21ed99e
Filipe Tavares-Cadete <filipe@cadete.eu>
2020-11-22 16:33:23 +0000

    Added comparison to all clusters.

--- main.R
+++ main.R
@@ -6,6 +6,7 @@ ctx <- tercenCtx()
 
 direction <- as.character(ctx$op.value('direction'))
 log_fold_change_threshold <- as.numeric(ctx$op.value('log_fold_change_threshold'))
+comparison_to_make <- as.character(ctx$op.value('comparison_to_make'))
 
 logged_count_matrix <- ctx$as.matrix()
 
@@ -14,16 +15,28 @@ clusters <- ctx$cselect()[[1]]
 markers_detected <- findMarkers(logged_count_matrix,
                                 clusters,
                                 direction = direction,
-                                lfc = log_fold_change_threshold)
+                                lfc = log_fold_change_threshold,
+                                pval.type = comparison_to_make)
 
 output_frame <- lapply(names(markers_detected),
-                       function(x) markers_detected[[x]] %>%
-                         as_tibble(rownames = ".ri") %>%
-                         mutate(cluster = x,
-                                .ri = as.integer(.ri) - 1) %>%
-                         select(marker_for_cluster = cluster, .ri, Top, FDR)) %>%
+                       function(x) {
+                         to_return <- markers_detected[[x]] %>%
+                           as_tibble(rownames = ".ri") %>%
+                           mutate(cluster = x,
+                                  .ri = as.integer(.ri) - 1)
+                         
+                         if (comparison_to_make == "any") {
+                           to_return <- to_return %>%
+                             select(marker_for_cluster = cluster, .ri, Top, FDR)
+                         } else if (comparison_to_make == "all") {
+                           to_return <- to_return %>%
+                             select(marker_for_cluster = cluster, .ri, FDR)
+                         }
+                         
+                         return(to_return)
+                       }) %>%
   bind_rows()
+
 
 ctx$addNamespace(output_frame) %>%
   ctx$save()
-
