91ad030a0a1d23dfe2fbd8e5c365c5cdddb159e9
agouy <contact@agouy.com>
2022-04-26 14:39:09 +0000

    renv++

--- main.R
+++ main.R
@@ -1,6 +1,8 @@
-library(tercen)
-library(dplyr)
-library(scran)
+suppressPackageStartupMessages(expr = {
+  library(tercen)
+  library(dplyr)
+  library(scran) 
+})
 
 ctx <- tercenCtx()
 
@@ -12,29 +14,37 @@ logged_count_matrix <- ctx$as.matrix()
 
 clusters <- ctx$cselect()[[1]]
 
-markers_detected <- findMarkers(logged_count_matrix,
-                                clusters,
-                                direction = direction,
-                                lfc = log_fold_change_threshold,
-                                pval.type = comparison_to_make)
+markers_detected <- findMarkers(
+  logged_count_matrix,
+  clusters,
+  direction = direction,
+  lfc = log_fold_change_threshold,
+  pval.type = comparison_to_make
+)
 
-output_frame <- lapply(names(markers_detected),
-                       function(x) {
-                         to_return <- markers_detected[[x]] %>%
-                           as_tibble(rownames = ".ri") %>%
-                           mutate(cluster = x,
-                                  .ri = as.integer(.ri) - 1)
-                         
-                         if (comparison_to_make == "any") {
-                           to_return <- to_return %>%
-                             select(marker_for_cluster = cluster, .ri, Top, FDR)
-                         } else if (comparison_to_make == "all") {
-                           to_return <- to_return %>%
-                             select(marker_for_cluster = cluster, .ri, FDR)
-                         }
-                         
-                         return(to_return)
-                       }) %>%
+output_frame <- lapply(
+  names(markers_detected), function(x) {
+    
+     to_return <- markers_detected[[x]] %>%
+       as_tibble(rownames = ".ri") %>%
+       mutate(cluster = x,
+              .ri = as.integer(.ri) - 1)
+     
+     if (comparison_to_make == "any") {
+       
+       to_return <- to_return %>%
+         select(marker_for_cluster = cluster, .ri, Top, FDR)
+       
+     } else if (comparison_to_make == "all") {
+       
+       to_return <- to_return %>%
+         select(marker_for_cluster = cluster, .ri, FDR)
+       
+     }
+     
+     return(to_return)
+     
+}) %>%
   bind_rows()
 
 
